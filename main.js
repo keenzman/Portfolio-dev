9485507d582f5da1be72c98ac5ab5421d435a44b
keenzman <szaman_03@hotmail.co.uk>
2021-05-11 16:20:47 +0100

    Parallax working

--- main.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener('scroll', function() {
-  const parallax = document.querySelector('.landing-header__img');
-  let scrollPosition = window.scrollY;
-
-  parallax.style.transform = 'translateY(' + scrollPosition *.5 + 'px)';
-})
