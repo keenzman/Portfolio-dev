6131d6a7119c2f926c0c5381c8dbc7ec23175d82
keenzman <szaman_03@hotmail.co.uk>
2021-05-12 05:50:29 +0100

    Fix Projects overflow bug

---
+++ main.js
@@ -0,0 +1,21 @@
+// Parallax effect on scroll
+window.addEventListener('scroll', function() {
+  const parallax = document.querySelector('.landing-header__img');
+  let scrollPosition = window.scrollY;
+
+  parallax.style.transform = 'translateY(' + scrollPosition *.5 + 'px)';
+})
+
+// carousel config with responsive breakpoints
+const config = {
+  type: 'slider',
+  breakpoints: {
+    1200: {
+      perView: 1,
+    },
+    800: {
+      perView: 1,
+    }
+  }
+}
+new Glide('.glide', config).mount()
